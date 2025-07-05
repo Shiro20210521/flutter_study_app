5cc4d8edf050e0f3f77831e3e152d73eba40736f
Shiro20210521 <sanshiro.tokuhisa@gmail.com>
2025-07-05 20:16:19 +0900

    draft_2

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -2,10 +2,12 @@ class Todo {
   final String id;
   final String title;
   bool isDone;
+  final DateTime? deadline;
 
   Todo({
     required this.id,
     required this.title,
     this.isDone = false,
+    this.deadline,
   });
 }
--- lib/screens/todo_edit_screen.dart
+++ lib/screens/todo_edit_screen.dart
@@ -5,7 +5,6 @@ import '../models/todo.dart';
 //編集対象のTodoをオプション引数(this.todo)で受け取る。Todoがnullなら新規モードになる
 class TodoEditScreen extends StatefulWidget {
   final Todo? todo;
-
   const TodoEditScreen({Key? key, this.todo}) : super(key: key);
 
   @override
@@ -15,6 +14,7 @@ class TodoEditScreen extends StatefulWidget {
 class _TodoEditScreenState extends State<TodoEditScreen> {
   //テキスト入力欄の状態を制御するためのコントローラー
   late TextEditingController _titleController;
+  DateTime? _selectedDate;
 
   @override
   void initState() {
@@ -30,7 +30,7 @@ class _TodoEditScreenState extends State<TodoEditScreen> {
     _titleController.dispose();
     super.dispose();
   }
-
+  //保存ボタンの処理
   void _saveTodo() {
     final title = _titleController.text.trim();
     if(title.isEmpty) return;
@@ -39,13 +39,31 @@ class _TodoEditScreenState extends State<TodoEditScreen> {
       id: const Uuid().v4(),
       title: title,
       isDone: widget.todo?.isDone ?? false,
+      deadline: _selectedDate,
     );
     //新しいTodoを返して画面を閉じる
     Navigator.pop(context, updateTodo);
   }
 
+  //期日設定処理
+  void _pickDate() async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2100),
+    );
+    if(picked != null){
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  //描画
   @override
   Widget build(BuildContext context) {
+    final selectedDate = _selectedDate;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.todo == null ? 'タスク追加' : 'タスク編集'),
@@ -60,6 +78,10 @@ class _TodoEditScreenState extends State<TodoEditScreen> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
+                onPressed: _pickDate,
+                child: Text(selectedDate == null? '期限を選択' : '期限: ${selectedDate.toLocal().toString().split(' ')[0]}'),
+            ),
+            ElevatedButton(
                 onPressed: _saveTodo,
                 child: Text('保存')
             )
@@ -68,5 +90,4 @@ class _TodoEditScreenState extends State<TodoEditScreen> {
       ),
     );
   }
-
 }
